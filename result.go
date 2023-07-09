06ff4abfc54b2d8512aacd811d428a41a3ffbc96
Mark <shalpack@gmail.com>
2023-07-09 11:36:53 +0300

    Conceptual refactor

--- result.go
+++ result.go
@@ -1,14 +1,5 @@
 package result
 
-import (
-	"errors"
-	"fmt"
-	"reflect"
-)
-
-type isOk bool
-type isErr bool
-
 type mFunc[T any, U any] func(T) U
 
 // Result is a container type that holds a value of type T or an error.
@@ -18,34 +9,39 @@ type mFunc[T any, U any] func(T) U
 // it will always return a zero value of type T.
 // In such cases, for example, calling Value() will return a zero value of type T,
 // and calling Match() will always return result.IsErr().
-type Result[T any] struct {
-	value   *T
-	err     error
-	variant interface{}
-	strict  bool
+type Result[T any, E any] struct {
+	value  *T
+	err    *E
+	strict bool
 }
 
+// C is an optional config struct used to create a new Result.
 type C struct {
-	strict bool
+	// Strict allows you to deny access to a Result value without using
+	// the Error() method first.
+	//
+	// Calling Value() or Unwrap() on Result will panic whether there is
+	// and error or not.
+	Strict bool
 }
 
 // Ok returns a new instance of Result[T] that holds a value, where T is
 // the inherited type of value.
 //
 // Calling Match() on such a Result will always match result.IsOk()
-func Ok[T any](value T, config ...C) Result[T] {
-	return ok(value, config...)
+func Ok[T any](value T, config ...C) Result[T, T] {
+	return ok[T, T](value, config...)
 }
 
 // Err returns a new instance of Result[T] that holds an error, where T is
 // the inherited type of zero value.
 //
 // Calling Match() on such a Result will always match result.IsErr()
-func Err[T any](rErr error, config ...C) Result[T] {
-	return err[T](rErr, config...)
+func Err[E any](rErr E, config ...C) Result[E, E] {
+	return err[E, E](rErr, config...)
 }
 
-// New returns a new instance of the Result[T], where T is
+// newResult returns an instance of the Result[T], where T is
 // the inherited type of value.
 //
 // If a non-nil rError was passed as an argument, Error() will return
@@ -57,12 +53,14 @@ func Err[T any](rErr error, config ...C) Result[T] {
 // The discriminated union nature of T | error is simulated using the Match() method,
 // which allows matching the current Result[T] with result.IsOk() in case of no error,
 // as well as with result.IsErr() in case of an error.
-func New[T any](value T, rError error, config ...C) Result[T] {
-	if rError != nil {
-		return err[T](rError, config...)
+func newResult[T, E any](value T, rError E, config ...C) Result[T, E] {
+	var isStrict bool
+
+	if len(config) > 0 {
+		isStrict = config[0].Strict
 	}
 
-	return ok(value, config...)
+	return Result[T, E]{value: &value, err: &rError, strict: isStrict}
 }
 
 // Match simulates pattern matching for handling scenarios of having a valid result
@@ -71,6 +69,7 @@ func New[T any](value T, rError error, config ...C) Result[T] {
 //
 //	v := 1
 //	r := result.New(1, nil)
+//
 //	switch r.Match() {
 //	case result.IsOk():
 //		fmt.Println("there is a value:")
@@ -79,96 +78,78 @@ func New[T any](value T, rError error, config ...C) Result[T] {
 //		fmt.Println("there is an error:")
 //		fmt.Println(x.Error())
 //	}
-func (r Result[T]) Match() reflect.Type {
-	return reflect.TypeOf(r.variant)
-}
-
-// Value returns the underlying value of type T.
-//
-// Will panic if Result was built with C{strict: true}
-func (r Result[T]) Value() T {
-	if r.strict {
-		panic("cannot use value of strict result unless error is checked")
-	}
-
+func Match[T, E any, U any](r Result[T, E], okF func(T) U, errF func(E) U) U {
 	if r.err != nil {
-		return *new(T)
+		return errF(r.innerError())
 	}
 
+	return okF(r.innerValue())
+}
+
+func (r Result[T, E]) innerValue() T {
 	return *r.value
 }
 
-func (r Result[T]) innerValue() T {
-	return *r.value
-}
+func (r Result[T, E]) innerError() E {
+	// if r.err != nil {
+	// 	return *r.err
+	// }
 
-// Error returns the underlying error.
-func (r Result[T]) Error() error {
-	return r.err
-}
-
-func (r Result[T]) innerError() error {
-	return r.err
+	// return *new(E)
+	return *r.err
 }
 
 // And returns a passed newR if the Result has no error and newR has no error.
 //
 // Otherwise returns a Result with a contained error, if present,
 // or a new Result with error from newR.
-func (r Result[T]) And(newR Result[T]) Result[T] {
+func (r Result[T, E]) And(newR Result[T, E]) Result[T, E] {
 	if r.err != nil {
-		return err[T](r.err)
+		return err[T, E](r.innerError())
 	}
 
 	if newR.err != nil {
-		return err[T](newR.err)
+		return err[T, E](newR.innerError())
 	}
 
-	return ok(newR.innerValue())
+	return ok[T, E](newR.innerValue())
 }
 
 // AndThen returns the result of f function with Result value as an argument if
 // Result has no error.
 //
 // Otherwise returns a Result with a contained error.
-func (r Result[T]) AndThen(f func(T) Result[T]) Result[T] {
+func (r Result[T, E]) AndThen(f func(T) Result[T, E]) Result[T, E] {
 	if r.err != nil {
-		return err[T](r.err)
+		return err[T, E](r.innerError())
 	}
 
 	return f(r.innerValue())
 }
 
-// Expect returns a new Result by wrapping a contained error with rErr if error is present.
-//
-// Otherwise returns self.
-func (r Result[T]) Expect(rErr error) Result[T] {
-	if r.err != nil {
-		return err[T](fmt.Errorf("%w: %w", rErr, r.err))
-	}
-
-	return r
-}
-
 // IsErr returns true if Result has an error.
-func (r Result[T]) IsErr() bool {
-	return r.Error() != nil
+func (r Result[T, E]) IsErr() bool {
+	return r.err != nil
 }
 
 // IsErrAnd returns true if rErr matches the contained Result error.
-func (r Result[T]) IsErrAnd(rErr error) bool {
-	return errors.Is(r.Error(), rErr)
+func (r Result[T, E]) IsErrAnd(rErr E) bool {
+	if r.err == nil {
+		return false
+	}
+
+	return r.err == &rErr
 }
 
 // IsOk returns true if Result has no error.
-func (r Result[T]) IsOk() bool {
-	return r.Error() == nil
+func (r Result[T, E]) IsOk() bool {
+	return r.err == nil
 }
 
 // IsOkAnd returns true if Result has no error and the contained value
 // matches a predicate of f.
-func (r Result[T]) IsOkAnd(f func(T) bool) bool {
-	if r.Error() != nil {
+func (r Result[T, E]) IsOkAnd(f func(T) bool) bool {
+	if r.err != nil {
 		return false
 	}
 
@@ -177,40 +158,44 @@ func (r Result[T]) IsOkAnd(f func(T) bool) bool {
 
 // Map returns a new Result by applying an f function to a Result value,
 // leaving Result error untouched.
-func Map[T, U any](r Result[T], f mFunc[T, U]) Result[U] {
-	return New[U](f(r.innerValue()), r.Error())
+func Map[T, E, U any](r Result[T, E], okF mFunc[T, U]) Result[U, E] {
+	computed := okF(r.innerValue())
+
+	return Result[U, E]{value: &computed, err: r.err}
 }
 
 // MapErr returns result of f function with Result error as an argument
 // if Result has an error.
 //
 // Otherwise returns self.
-func (r Result[T]) MapErr(f func(error) error) Result[T] {
-	if r.Error() == nil {
+func (r Result[T, E]) MapErr(errF func(E) E) Result[T, E] {
+	if r.err == nil {
 		return r
 	}
 
-	return New(r.innerValue(), f(r.Error()))
+	computed := errF(r.innerError())
+
+	return Result[T, E]{value: r.value, err: &computed}
 }
 
-// MapOr returns the provided v of type T if Result has an error, otherwise
+// MapOr returns the provided rDefault of type T if Result has an error, otherwise
 // returns a result of f function with Result value as an argument.
-func (r Result[T]) MapOr(v T, f func(T) T) T {
-	if r.Error() != nil {
-		return v
+func (r Result[T, E]) MapOr(rDefault T, f func(T) T) T {
+	if r.err != nil {
+		return rDefault
 	}
 
 	return f(r.innerValue())
 }
 
-// MapOrElse calls an eF function with Result error as an argument if Result
-// has an error, otherwise calls an oF function with a Result value as an argument.
-func (r Result[T]) MapOrElse(eF func(error) T, oF func(T) T) T {
-	if r.Error() != nil {
-		return eF(r.Error())
+// MapOrElse calls an errF function with Result error as an argument if Result
+// has an error, otherwise calls an okF function with a Result value as an argument.
+func (r Result[T, E]) MapOrElse(errF func(E) T, okF func(T) T) T {
+	if r.err != nil {
+		return errF(r.innerError())
 	}
 
-	return oF(r.innerValue())
+	return okF(r.innerValue())
 }
 
 // Unwrap allows obtaining the nested value and error inside the Result as
@@ -219,41 +204,50 @@ func (r Result[T]) MapOrElse(eF func(error) T, oF func(T) T) T {
 // It is recommended to use the Match() method for proper pattern matching.
 //
 // Will panic if Result was built with C{strict: true}
-func (r Result[T]) Unwrap() (T, error) {
-	return r.Value(), r.Error()
+func (r Result[T, E]) Unwrap() (T, E) {
+	if r.strict {
+		panic("cannot unwrap value and error of strict result")
+	}
+	return r.innerValue(), r.innerError()
 }
 
-// IsStrict returns whether it is mandatory to check for an error in Result[T]
-func (r Result[T]) IsStrict() bool {
+func (r Result[T, E]) UnwrapOr(rDefault T) T {
+	if r.err != nil {
+		return rDefault
+	}
+
+	return r.innerValue()
+}
+
+func (r Result[T, E]) UnwrapOrElse(f func(E) T) T {
+	if r.err == nil {
+		return r.innerValue()
+	}
+
+	return f(r.innerError())
+}
+
+// IsStrict returns whether calling Unwrap() will panic or not.
+func (r Result[T, E]) IsStrict() bool {
 	return r.strict
 }
 
-// IsOk returns the type to match with Match() for valid cases.
-func IsOk() reflect.Type {
-	return reflect.TypeOf(new(isOk))
-}
-
-// IsErr returns the type to match with Match() for invalid cases.
-func IsErr() reflect.Type {
-	return reflect.TypeOf(new(isErr))
-}
-
-func ok[T any](value T, config ...C) Result[T] {
+func ok[T, E any](value T, config ...C) Result[T, E] {
 	var isStrict bool
 
 	if len(config) > 0 {
-		isStrict = config[0].strict
+		isStrict = config[0].Strict
 	}
 
-	return Result[T]{value: &value, variant: new(isOk), strict: isStrict}
+	return Result[T, E]{value: &value, strict: isStrict}
 }
 
-func err[T any](err error, config ...C) Result[T] {
+func err[T, E any](err E, config ...C) Result[T, E] {
 	var isStrict bool
 
 	if len(config) > 0 {
-		isStrict = config[0].strict
+		isStrict = config[0].Strict
 	}
 
-	return Result[T]{err: err, variant: new(isErr), strict: isStrict}
+	return Result[T, E]{err: &err, strict: isStrict}
 }
--- result_test.go
+++ result_test.go
@@ -1,7 +1,6 @@
 package result
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,49 +9,49 @@ import (
 func TestNew(t *testing.T) {
 	type args struct {
 		value  int
-		rError error
+		rError int
 		c      C
 	}
 	tests := []struct {
 		name      string
 		args      args
 		wantValue int
-		wantError error
+		wantError int
 		wantPanic bool
 	}{
 		{
 			name:      "Result with value and empty error",
-			args:      args{value: 1, rError: nil},
+			args:      args{value: 1, rError: *new(int)},
 			wantValue: 1,
-			wantError: nil,
+			wantError: *new(int),
 			wantPanic: false,
 		},
 		{
 			name:      "Result with error and empty value",
-			args:      args{value: *new(int), rError: errors.New("oops, we'got problems")},
+			args:      args{value: *new(int), rError: *new(int)},
 			wantValue: *new(int),
-			wantError: errors.New("oops, we'got problems"),
+			wantError: *new(int),
 			wantPanic: false,
 		},
 		{
 			name:      "Result with error and value",
-			args:      args{value: 1, rError: errors.New("oops, we'got problems")},
-			wantValue: *new(int),
-			wantError: errors.New("oops, we'got problems"),
+			args:      args{value: 1, rError: 1},
+			wantValue: 1,
+			wantError: 1,
 			wantPanic: false,
 		},
 		{
 			name:      "Result with empty error and empty value",
-			args:      args{value: *new(int), rError: nil},
+			args:      args{value: *new(int), rError: *new(int)},
 			wantValue: *new(int),
-			wantError: nil,
+			wantError: *new(int),
 			wantPanic: false,
 		},
 		{
 			name:      "Result with strict config",
-			args:      args{value: 1, rError: nil, c: C{strict: true}},
+			args:      args{value: 1, rError: 1, c: C{Strict: true}},
 			wantValue: 1,
-			wantError: nil,
+			wantError: 1,
 			wantPanic: true,
 		},
 	}
@@ -60,14 +59,15 @@ func TestNew(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			defer func() { recover() }()
 
-			got := New(tt.args.value, tt.args.rError, tt.args.c)
+			got := newResult(tt.args.value, tt.args.rError, tt.args.c)
 
 			if tt.wantPanic {
-				assert.Panics(t, func() { got.Value() })
+				assert.Panics(t, func() { got.Unwrap() })
 			}
 
-			assert.Equal(t, tt.wantValue, got.Value())
-			assert.Equal(t, tt.wantError, got.Error())
+			value, err := got.Unwrap()
+			assert.Equal(t, tt.wantValue, value)
+			assert.Equal(t, tt.wantError, err)
 		})
 	}
 }
